9a8376fe8d07c7416cf523f31e59d1b0eed5f48a
Jean <33819199+jeantanzj@users.noreply.github.com>
2020-04-09 21:29:30 +0800

    Redirect from home to upload if logged in

--- app/routes.js
+++ app/routes.js
@@ -1,16 +1,25 @@
 const choo = require('choo');
 const download = require('./ui/download');
 const body = require('./ui/body');
+const homeSignin = require('./ui/homeSignin')
+const upload = require('./ui/home')
+
+function auth (onAuth, onPublic) {
+  return function (state, emit){
+    return (state.user.isSignedInWithOtp() ? onAuth : onPublic)(...arguments)
+  }
+}
+
 module.exports = function(app = choo({ hash: true })) {
-  app.route('/', body(require('./ui/homeSignin')));
+  app.route('/', auth(body(upload), body(homeSignin)));
   app.route('/download/:id', body(download));
   app.route('/download/:id/:key', body(download));
   app.route('/unsupported/:reason', body(require('./ui/unsupported')));
   app.route('/legal', body(require('./ui/legal')));
   app.route('/error', body(require('./ui/error')));
   app.route('/blank', body(require('./ui/blank')));
-  app.route('/login', body(require('./ui/homeSignin')));
+  app.route('/login', body(homeSignin));
   app.route('*', body(require('./ui/notFound')));
-  app.route('/upload', body(require('./ui/home')));
+  app.route('/upload', body(upload));
   return app;
 };
